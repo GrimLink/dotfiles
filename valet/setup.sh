7e2706ce3db5435f63f3390a325959bd2747c952
GrimLink <sean.grimlink@gmail.com>
2018-12-28 19:04:16 +0100

    IMP: valet setup script

--- valet/setup.sh
+++ valet/setup.sh
@@ -1,21 +1,26 @@
+#!/bin/bash
+
 # Get and install
-composer global require weprovide/valet-plus
+if ! command -v composer &> /dev/null; then
+  composer global require weprovide/valet-plus
+  valet fix
+  valet install --with-mariadb
 
-valet fix
-valet install --with-mariadb
+  # Create dev ENV
+  mkdir ~/projects
+  valet park ~/projects
 
-# Create dev ENV
-mkdir ~/projects
-valet park ~/projects
+  mkdir ~/projects/react
+  mkdir ~/projects/vue
 
-mkdir ~/projects/react
-mkdir ~/projects/vue
+  mkdir ~/projects/magento
+  valet park ~/projects/magento
 
-mkdir ~/projects/magento
-valet park ~/projects/magento
+  mkdir ~/projects/wordpress
+  valet park ~/projects/wordpress
 
-mkdir ~/projects/wordpress
-valet park ~/projects/wordpress
-
-mkdir ~/projects/joomla
-valet park ~/projects/joomla
+  mkdir ~/projects/joomla
+  valet park ~/projects/joomla
+else
+  echo -e "Composer is missing, can't install valet"
+fi
