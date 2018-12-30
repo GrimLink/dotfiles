a896df76ba2a8354794ae5ee2c8e8b1549fee8ea
GrimLink <sean.grimlink@gmail.com>
2018-12-31 00:38:49 +0100

    ADD: vue cli and npm init config

--- node/setup.sh
+++ node/setup.sh
@@ -7,8 +7,12 @@ if command -v node &> /dev/null; then
   # CLI's
   npm i -g grunt-cli
   npm i -g gulp-cli
+  npm i -g @vue/cli
   npm i -g np
+  # Base config
   npm config set depth 0
+  npm config set init-author-name "$(git config --global --get user.name)"
+  npm config set init-license "MIT"
 else
   echo -e "NODE is not installed\nPlease first install node to install Node Tools"
 fi
