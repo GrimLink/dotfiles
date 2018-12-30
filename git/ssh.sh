ed83d74fdde8ed52001417668921d391f28a47bf
GrimLink <sean.grimlink@gmail.com>
2018-12-31 00:08:08 +0100

    ADD: ssh script

---
+++ git/ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create default ssh key
+ssh-keygen -o -t rsa -b 4096 -C "$(git config --global --get user.email)" && /
+eval `ssh-agent` && /
+ssh-add -K ~/.ssh/id_rsa && /
+touch ~/.ssh/config && /
+echo "Host *" >> ~/.ssh/config && /
+echo "  AddKeysToAgent yes" >> ~/.ssh/config && /
+echo "  UseKeychain yes" >> ~/.ssh/config && /
+echo "  IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config && /
+pbcopy < ~/.ssh/id_rsa.pub
