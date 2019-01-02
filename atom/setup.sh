c8316a592ee78709d089178d85187697b630fb1b
allrude <allrude@gmail.com>
2019-01-02 18:52:13 +0100

    IMP: setup for Atom

--- atom/setup.sh
+++ atom/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Install all extensions
-if ! command -v apm &> /dev/null; then
+if command -v apm &> /dev/null; then
   apm stars --user GrimLink --install
+else
+  echo "apm cli missing";
 fi
 
 # Sync config
