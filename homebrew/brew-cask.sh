1e2eb0de036db4fe3e64af7033f99dd5612ef9a7
GrimLink <sean.grimlink@gmail.com>
2019-01-05 20:09:16 +0100

    IMP: brew cask to array

--- homebrew/brew-cask.sh
+++ homebrew/brew-cask.sh
@@ -5,26 +5,30 @@ if ! command -v brew &> /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Apps
-brew cask install firefox-developer-edition
-brew cask install hyper
-brew cask install sequel-pro
-brew cask install tower
-brew cask install transmit
-brew cask install vlc
-brew cask install dropbox
-brew cask install boostnote
-## Editors
-brew cask install visual-studio-code
-brew cask install atom
-brew cask install graphiql
-brew cask install postman
-## Communication
-brew cask install slack
-brew cask install skype
-## Utils
-brew cask install google-backup-and-sync
-brew cask install imageoptim
-brew cask install appcleaner
-brew cask install keka
-brew cask install keepingyouawake
+APPS=(
+  hyper
+  sequel-pro
+  tower
+  transmit
+  vlc
+  dropbox
+  boostnote
+  # Editors
+  visual-studio-code
+  atom
+  graphiql
+  postman
+  # Communication
+  slack
+  skype
+  # Utils
+  google-backup-and-sync
+  imageoptim
+  appcleaner
+  keka
+  keepingyouawake
+)
+
+for i in "${APPS[@]}"; do
+  brew cask install $i
+done
