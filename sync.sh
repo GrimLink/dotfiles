56d19422dbb48a7899d2c44f6f21c384c35eaac8
GrimLink <sean.grimlink@gmail.com>
2019-02-02 22:41:28 +0100

    IMP: sync cmd with flag for npm update

--- sync.sh
+++ sync.sh
@@ -12,7 +12,15 @@ source bin/setup.sh;
 
 source git/setup.sh;
 
-source node/setup.sh;
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  source node/setup.sh;
+else
+  read -p "Update npm global pkg's. Are you sure? (y) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    source node/setup.sh;
+  fi;
+fi;
 
 source vscode/extensions.sh;
 source vscode/config.sh;
