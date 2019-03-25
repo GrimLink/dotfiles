323cfd7e07f34ade78a8b9b39216eb72280b6066
GrimLink <sean.grimlink@gmail.com>
2019-03-25 07:00:47 +0100

    ADD: if to valet setup for the folder create

--- valet-plus/setup.sh
+++ valet-plus/setup.sh
@@ -2,25 +2,25 @@
 
 # Get and install
 if command -v composer &> /dev/null; then
-  composer global require weprovide/valet-plus
-  valet fix
-  valet install --with-mariadb
+  composer global require weprovide/valet-plus &&
+  valet fix &&
+  valet install --with-mariadb;
 
-  # Create dev ENV
-  mkdir ~/projects
-  valet park ~/projects
+  read -p "Create project folders & Valet park? (y) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    mkdir ~/projects;
+    valet park ~/projects;
 
-  mkdir ~/projects/react
-  mkdir ~/projects/vue
+    mkdir ~/projects/react;
+    mkdir ~/projects/vue;
 
-  mkdir ~/projects/magento
-  valet park ~/projects/magento
+    mkdir ~/projects/magento;
+    valet park ~/projects/magento;
 
-  mkdir ~/projects/wordpress
-  valet park ~/projects/wordpress
-
-  mkdir ~/projects/joomla
-  valet park ~/projects/joomla
+    mkdir ~/projects/wordpress;
+    valet park ~/projects/wordpress;
+  fi
 else
   echo "Composer is missing, can't install valet" && /
   echo "Run the dotfiles/valet/setup.sh, when you're ready"
