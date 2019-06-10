ae4f8054f9f9953e8e37a99a7336506efae84ed6
GrimLink <sean.grimlink@gmail.com>
2019-06-10 19:39:48 +0200

    IMP: fix chmox on magerun and allow bit n98 and none versions

--- magerun/setup.sh
+++ magerun/setup.sh
@@ -4,11 +4,13 @@
 # Instead the one used by valet
 
 # Magerun
-curl -sS -O https://files.magerun.net/n98-magerun.phar &&
-  chmod +x n98-magerun.phar &&
-  mv -f n98-magerun.phar /usr/local/bin/magerun
+curl -sS -O https://files.magerun.net/n98-magerun.phar
+mv -f n98-magerun.phar /usr/local/bin/n98-magerun
+chmod +x /usr/local/bin/n98-magerun
+ln -s /usr/local/bin/n98-magerun /usr/local/bin/magerun
 
 # Magerun2
-curl -sS -O https://files.magerun.net/n98-magerun2.phar &&
-  chmod +x n98-magerun2.phar &&
-  mv -f n98-magerun2.phar /usr/local/bin/magerun2
+curl -sS -O https://files.magerun.net/n98-magerun2.phar
+mv -f n98-magerun2.phar /usr/local/bin/n98-magerun2
+chmod +x /usr/local/bin/n98-magerun2
+ln -s /usr/local/bin/n98-magerun2 /usr/local/bin/magerun2
