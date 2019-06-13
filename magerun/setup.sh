0dde562d38cbcc567c9758b9affb008de8de0f9d
GrimLink <sean.grimlink@gmail.com>
2019-06-13 23:57:25 +0200

    IMP: comment

--- magerun/setup.sh
+++ magerun/setup.sh
@@ -3,13 +3,13 @@
 # Download our own magerun
 # Instead the one used by valet
 
-# Magerun
+echo "Installing Magerun"
 curl -sS -O https://files.magerun.net/n98-magerun.phar
 mv -f n98-magerun.phar /usr/local/bin/n98-magerun
 chmod +x /usr/local/bin/n98-magerun
 ln -s /usr/local/bin/n98-magerun /usr/local/bin/magerun
 
-# Magerun2
+echo "Installing Magerun2"
 curl -sS -O https://files.magerun.net/n98-magerun2.phar
 mv -f n98-magerun2.phar /usr/local/bin/n98-magerun2
 chmod +x /usr/local/bin/n98-magerun2
