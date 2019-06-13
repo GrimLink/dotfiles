9930c5963144f879108b63ada5f32588c1f58535
GrimLink <sean.grimlink@gmail.com>
2019-06-13 23:55:25 +0200

    IMP: brew scripts

---
+++ brew/app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install it, if not already
+if ! command -v brew &>/dev/null; then
+  echo "Homebrew not found, installing"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if command -v brew &>/dev/null; then
+  brew cask install hyper
+  brew cask install sequel-pro
+  brew cask install tableplus
+  brew cask install tower
+  brew cask install transmit
+  brew cask install vlc
+  brew cask install dropbox
+  brew cask install boostnote
+  # Editors
+  brew cask install visual-studio-code
+  brew cask install graphiql
+  brew cask install postman
+  # Communication
+  brew cask install slack
+  brew cask install discord
+  brew cask install skype
+  brew cask install whatsapp
+  # Utils
+  brew cask install google-backup-and-sync
+  brew cask install imageoptim
+  brew cask install appcleaner
+  brew cask install keka
+  brew cask install keepingyouawake
+fi
---
+++ brew/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install it, if not already
+if ! command -v brew &>/dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Run brew install pkg's
+if ! command -v brew &>/dev/null; then
+  # Install node & yarn
+  brew install yarn
+  # install nvm
+  brew install nvm
+  # Install php + latest stable 3 henkrehorst/php versions
+  brew install php
+  brew tap henkrehorst/php
+  brew install valet-php@7.1
+  brew install valet-php@7.2
+  brew install valet-php@7.3
+  # Install javajdk8 (osx needed version) + elasticsearch that needs java
+  brew cask install homebrew/cask-versions/adoptopenjdk8
+  brew install elasticsearch@5.6
+  # Install composer
+  brew install composer
+  # Install image converter
+  brew install imagemagick
+else
+  echo "brew is not installed"
+fi
