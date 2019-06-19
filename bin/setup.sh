3458e62b9adcfdafd03696d0ab95b757fe953700
GrimLink <sean.grimlink@gmail.com>
2019-06-19 21:20:27 +0200

    IMP: comments in bin sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -19,16 +19,22 @@ function rsyncBin() {
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   rsyncBin
+
   echo -e "${YELLOW}Getting mage${RESET}"
   curl -sS -O $GIT_URL/mage/master/mage
+
+  echo -e "${YELLOW}Getting create-project${RESET}"
   bash -c "$(curl -LsS https://raw.githubusercontent.com/GrimLink/create-project/master/install.sh)"
 else
   read -p "Update bin files? (y) " -n 1
   echo ""
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     rsyncBin
+
     echo -e "${YELLOW}Getting mage${RESET}"
     curl -sS -O $GIT_URL/mage/master/mage
+
+    echo -e "${YELLOW}Getting create-project${RESET}"
     bash -c "$(curl -LsS https://raw.githubusercontent.com/GrimLink/create-project/master/install.sh)"
   fi
 fi
