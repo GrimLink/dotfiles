d97228a2678a53b59cbd8f63de1d5dc743e3c94a
GrimLink <sean.grimlink@gmail.com>
2019-06-20 23:28:41 +0200

    ADD: bash setup script

---
+++ bash/setup.sh
@@ -0,0 +1,13 @@
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  rsync -avh --no-perms \
+    "$(dirname "${BASH_SOURCE}")/.bash_profile" \
+    "$(dirname "${BASH_SOURCE}")/.bash_prompt" ~
+else
+  read -p "Update Bash config files. Are you sure? (Y/n) " -n 1
+  echo ""
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rsync -avh --no-perms \
+      "$(dirname "${BASH_SOURCE}")/.bash_profile" \
+      "$(dirname "${BASH_SOURCE}")/.bash_prompt" ~
+  fi
+fi
