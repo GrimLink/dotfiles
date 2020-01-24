ea96c54a9e58fe27acc238610d664b2a2fdd58d9
GrimLink <sean.grimlink@gmail.com>
2020-01-24 22:38:52 +0100

    ADD: gatsby and gridsome to g pkg's

--- node/pkg.sh
+++ node/pkg.sh
@@ -4,6 +4,8 @@
 if command -v node &>/dev/null; then
   npm i -g gulp-cli
   npm i -g @vue/cli
+  npm i -g @gridsome/cli
+  npm i -g gatsby-cli
   npm i -g np
 else
   echo "Skipping.. NODE is not installed"
