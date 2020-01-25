4353c2cc2ccea29d7410b97711a91e718ce6a81f
GrimLink <sean.grimlink@gmail.com>
2020-01-25 13:57:23 +0100

    ADD: audit tools back

--- node/pkg.sh
+++ node/pkg.sh
@@ -2,10 +2,15 @@
 
 # Installs all node Tools
 if command -v node &>/dev/null; then
+  # Auditing (using npx instead)
+  npm i -g lighthouse
+  npm i -g hint
+  # Compilers/Bundlers
   npm i -g gulp-cli
   npm i -g @vue/cli
   npm i -g @gridsome/cli
   npm i -g gatsby-cli
+  # Node Tools
   npm i -g np
 else
   echo "Skipping.. NODE is not installed"
