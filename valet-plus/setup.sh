e8b5b060e1e4b71f304a1685ae54f0b7f0b251ee
Grimlink <sean.grimlink@gmail.com>
2020-10-11 15:21:40 +0200

    IMP: valet setup by splitting the project (folder) setup

--- valet-plus/setup.sh
+++ valet-plus/setup.sh
@@ -14,23 +14,9 @@ if command -v composer &>/dev/null; then
   composer global require weprovide/valet-plus &&
   valet fix &&
   valet install --with-mariadb
-
-  read -p "Create project folders & Valet park? [Y/n] " -n 1
-  echo ""
-  if [[ ! $REPLY =~ ^[Nn]$ ]]; then
-    mkdir ~/projects
-    valet park ~/projects
-
-    mkdir ~/projects/react
-    mkdir ~/projects/vue
-
-    mkdir ~/projects/magento
-    valet park ~/projects/magento
-
-    mkdir ~/projects/wordpress
-    valet park ~/projects/wordpress
-  fi
 else
-  echo "Composer is missing, can't install valet" && /
-  echo "Run the dotfiles/valet/setup.sh, when you're ready"
+  echo "Composer is missing, can't install valet" && exit 1;
 fi
+
+# Setup project folder
+source $(dirname "${BASH_SOURCE}")/project.sh
