d10b194fa7771bf96327f944047e63fecf633222
Grimlink <sean.grimlink@gmail.com>
2020-10-11 15:20:58 +0200

    IMP: move valet specific cmds to the valet setup

--- valet-plus/setup.sh
+++ valet-plus/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # Get and install
+if ! command -v brew &>/dev/null; then
+  brew tap henkrehorst/php
+  brew install valet-php@7.2 # For valet install
+  brew install valet-php@7.3
+  brew install valet-php@7.4
+else
+  echo "Brew is missing, can't install valet" && exit 1;
+fi
+
 if command -v composer &>/dev/null; then
   composer global require weprovide/valet-plus &&
   valet fix &&
