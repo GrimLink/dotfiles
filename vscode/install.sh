0e094f9c151d30b1f84dcdfedf21c552a694e5a5
grimlink <sean.grimlink@gmail.com>
2022-02-01 21:06:05 +0100

    IMP: make vscode the default editor

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,6 +2,8 @@
 
 if command -v brew &>/dev/null; then
   brew install --cask visual-studio-code
+
+  defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.microsoft.VSCode;}'
 else
   echo "Can't install vscode, brew is not installed"
 fi
