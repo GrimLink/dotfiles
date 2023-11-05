51d7e3acaa95e61a7e52cf107f2cca61be1f9385
Sean van Zuidam <sean.grimlink@gmail.com>
2023-11-05 13:04:25 +0100

    FIX: nvm steps

--- nvm/install.sh
+++ nvm/install.sh
@@ -7,13 +7,7 @@ function StepSection() {
   echo -e "${GREEN}$@${RESET}"
 }
 
-StepSection "Instaling nvm"
-git clone https://github.com/lukechilds/zsh-nvm.git "$HOME/.zsh/nvm"
-# Reload shell, to take effect from adding nvm
-# The .zshrc contains the loader for nvm
-exec $SHELL -l
-
 StepSection "Setting nvm defaults"
-mkdir ~/.nvm
+mkdir $HOME/.nvm
 nvm alias default system
 nvm install --lts
