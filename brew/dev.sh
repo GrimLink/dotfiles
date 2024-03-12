a74934d2eab64e7e1b1e4e0182052f7388966419
Sean van Zuidam <sean.grimlink@gmail.com>
2024-03-12 10:20:23 +0100

    Update dev.sh with todo for mailhog setup

--- brew/dev.sh
+++ brew/dev.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 brew install gh
-brew install mailhog
 brew install imagemagick
+
+# Mailhog
+brew install mailhog
+
+# TODO add `conf.d/smtp-mailhog.ini` to the instructions for both homebrew paths
+# ```ini
+# ; Use Mailhog for mail delivery
+# sendmail_path=/opt/homebrew/bin/mailhog sendmail
+# ```
 
 # Frameworks specific tools
 brew install wp-cli
