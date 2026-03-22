c8324cdf30a1433e26086e42a5fa0446d9aa9836
grimlink <sean@hyva.io>
2026-03-22 22:53:08 +0100

    ADD: Database GUI tools (TablePlus and Sequel Ace)

--- brew/dev.sh
+++ brew/dev.sh
@@ -3,15 +3,6 @@
 brew install gh
 brew install imagemagick
 
-# Mailhog
-brew install mailhog
-
-# TODO add `conf.d/smtp-mailhog.ini` to the instructions for both homebrew paths
-# ```ini
-# ; Use Mailhog for mail delivery
-# sendmail_path=/opt/homebrew/bin/mailhog sendmail
-# ```
-
-# Frameworks specific tools
-# brew install wp-cli
-# brew install FriendsOfShopware/tap/shopware-cli
+# Databases
+brew install --cask tableplus
+brew install --cask sequel-ace
