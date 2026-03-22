047c1ddcc58f40d29356c1c0757b315158145d8d
grimlink <sean@hyva.io>
2026-03-22 23:30:38 +0100

    IMP: use hardcoded default

--- brew/node.sh
+++ brew/node.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-DEFAULT_NAME=$(git config --global --get user.name);
-
 brew install node
 brew install pnpm
 
 # Setup default config
-if [[ -z "$DEFAULT_NAME" ]]; then
-  read -p "Author: " NAME && echo ""
-else
-  NAME=$DEFAULT_NAME
-fi
-
 npm config set depth 0
-npm config set init-author-name "${NAME}"
+npm config set init-author-name "GrimLink"
 npm config set init-license "MIT"
 echo "tag-version-prefix=" > ~/.npmrc
