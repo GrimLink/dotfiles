786bca3d0c31bd594cb1bbd241d6296df07ff490
grimlink <sean@hyva.io>
2026-03-29 19:37:12 +0200

    IMP: Detect shell profile and support both bash and zsh

--- brew/install.sh
+++ brew/install.sh
@@ -4,14 +4,18 @@
 RESET='\033[0m'
 GREEN='\033[1;32m'
 
+SHELL_PROFILE="$HOME/.bash_profile"
+[ -f "$HOME/.zshrc" ] && SHELL_PROFILE="$HOME/.zprofile"
+
 echo -e "\n${GREEN}Installing homebrew (https://brew.sh/)${RESET}"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Add custom bin path if Homebrew is installed in 'opt'
 if [[ -d /opt/homebrew ]]; then
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$SHELL_PROFILE"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
+  echo 'eval "$(/usr/local/bin/brew shellenv)"' >> "$SHELL_PROFILE"
   eval "$(/usr/local/bin/brew shellenv)"
 fi
 
