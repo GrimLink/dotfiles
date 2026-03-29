76fb711da13322db6b0ef7a4ae5245f11b5db155
grimlink <sean@hyva.io>
2026-03-29 22:20:18 +0200

    ADD: VS Code extensions list and installation script

--- _Library/vscode/install.sh
+++ _Library/vscode/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 brew install --cask visual-studio-code
+
+read -p "Install extensions? (Y/n): " INSTALL_EXTENSIONS
+if [[ "$INSTALL_EXTENSIONS" == "" || "$INSTALL_EXTENSIONS" == "y" || "$INSTALL_EXTENSIONS" == "yes" ]]; then
+  EXTENSIONS_FILE="$(dirname "${BASH_SOURCE}")/extensions"
+  while IFS= read -r line; do
+    [[ -z "$line" || "$line" == \#* ]] && continue
+    code --install-extension "$line"
+  done < "$EXTENSIONS_FILE"
+fi
