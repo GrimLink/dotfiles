668da1c13da1ef3361d86e0e1e369417a5c87e01
grimlink <sean@hyva.io>
2026-03-29 18:32:38 +0200

    IMP: Switch default editor from VS Code to Zed
    Consolidate editor environment variable configuration
    into shell/exports and remove redundant shell RC
    detection from install scripts.

--- _Library/vscode/install.sh
+++ _Library/vscode/install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-SHELL_RC="$HOME/.bashrc"
-[ -f "$HOME/.zshrc" ] && SHELL_RC="$HOME/.zshrc"
-
 brew install --cask visual-studio-code
-
-echo "export EDITOR=\"code -w\"" >> "$SHELL_RC"
-echo "export VISUAL=\"code -w\"" >> "$SHELL_RC"
--- zed/install.sh
+++ zed/install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-SHELL_RC="$HOME/.bashrc"
-[ -f "$HOME/.zshrc" ] && SHELL_RC="$HOME/.zshrc"
-
 brew install --cask zed
-
-echo "export EDITOR=\"zed -w\"" >> "$SHELL_RC"
-echo "export VISUAL=\"zed -w\"" >> "$SHELL_RC"
