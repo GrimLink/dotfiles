6ba193f55457595553a6afe599e5c94188af1dc5
Sean van Zuidam <sean.grimlink@gmail.com>
2026-06-01 09:52:01 +0200

    Set npm to ignore scripts during installation

--- node/install.sh
+++ node/install.sh
@@ -9,4 +9,6 @@ npm config set init-author-name "GrimLink"
 npm config set init-license "MIT"
 npm config set init-type "module"
 npm config set min-release-age 3
+npm config set ignore-scripts true
+
 echo "tag-version-prefix=" > ~/.npmrc
